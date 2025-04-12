000f3bf0e8714622f648669d865167b238a54736
CaroCrespo <carocrespo1986@gmail.com>
2025-04-11 21:22:16 -0300

    7 checked in terminal

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -45,9 +45,9 @@ cp ./data/raw/*event*.log ./data/processed/event_logs/
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 rm ./data/raw/*ipaddr*
 rm ./data/processed/user_logs/*ipaddr*
-ls data/raw/*ipaddr*
-# 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 
+# 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
+ls ./data/processed/*/ >> ./data/inventory.txt
 
 ###########################################
 
